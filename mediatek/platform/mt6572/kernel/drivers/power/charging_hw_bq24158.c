7f48069851f021779d08c67911ce366defc5c755
LG-L60 <vovka-z@ya.ru>
2016-04-14 00:03:05 +0300

    charger: Исправляем параметры заряда и защиты батареи

--- mediatek/platform/mt6572/kernel/drivers/power/charging_hw_bq24158.c
+++ mediatek/platform/mt6572/kernel/drivers/power/charging_hw_bq24158.c
@@ -498,31 +498,28 @@ static void hw_bc11_dump_register(void)
     battery_xlog_printk(BAT_LOG_FULL, "gpio_number=0x%x,gpio_on_mode=%d,gpio_off_mode=%d\n", gpio_number, gpio_on_mode, gpio_off_mode);
 	
 	upmu_set_rg_usbdl_rst(1);		//force leave USBDL mode
-/*
+#if defined(ARIMA_LO1_HW)
+    if (BATTERY_PACK_BL_44JH == battery_pack_id)
+        bq24158_config_interface_liao(0x06,0x27); //=> 0x2A => 0x27, safety current= 0.91A, voltage=4.4V => 4.34V
+    else //BATTERY_PACK_BL_44JN
+        bq24158_config_interface_liao(0x06,0x20); //=> 0x28 => 0x20, safety current= 0.91A, safety voltage=4.36V => 4.2V
+#else
 	#if defined(HIGH_BATTERY_VOLTAGE_SUPPORT)
         bq24158_config_interface_liao(0x06,0x77); // ISAFE = 1250mA, VSAFE = 4.34V
     #else
         bq24158_config_interface_liao(0x06,0x70);
 	#endif
-*/	    
+#endif
     bq24158_config_interface_liao(0x00,0xC0);	//kick chip watch dog
     bq24158_config_interface_liao(0x01,0xb8);	//TE=1, CE=0, HZ_MODE=0, OPA_MODE=0
 //<2014/05/04-tedwu, Customize Battery/Charging parameters.
-    bq24158_config_interface_liao(0x05,0x04);  //special voltage = 4.52V
-#if defined(ARIMA_LO1_HW)
-    if (BATTERY_PACK_BL_44JH == battery_pack_id)
-        bq24158_config_interface_liao(0x04,0x1A); //146mA
-    else //BATTERY_PACK_BL_44JN
-        bq24158_config_interface_liao(0x04,0x19); //146mA //termination current=121mA
-#else
-    bq24158_config_interface_liao(0x04,0x1A); //146mA
-#endif
+    bq24158_config_interface_liao(0x05,0x03);  //special voltage = 4.44V
 	if ( !charging_init_flag ) {   
 #if defined(ARIMA_LO1_HW)
         if (BATTERY_PACK_BL_44JH == battery_pack_id)
-            bq24158_config_interface_liao(0x04,0x1A); //146mA
+            bq24158_config_interface_liao(0x04,0x19); //termination current=121mA
         else //BATTERY_PACK_BL_44JN
-            bq24158_config_interface_liao(0x04,0x19); //146mA //termination current=121mA
+            bq24158_config_interface_liao(0x04,0x10); //termination current=50mA
 #else
         bq24158_config_interface_liao(0x04,0x1A); //146mA
 #endif
